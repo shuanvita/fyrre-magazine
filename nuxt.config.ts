3412f4e047e55d8f0ae8984b37adf8ec8d24c15c
Andrey Shurygin <shuanvita@gmail.com>
2025-06-18 20:15:27 +0300

    Initial project

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  srcDir: 'app/',
+  future: {
+    compatibilityVersion: 4,
+  },
+  css: ['./app/assets/css/global.css'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+})
