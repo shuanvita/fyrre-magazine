ea35010aef10ec8c1f4c61291e6e0973869e4d86
Andrey Shurygin <shuanvita@gmail.com>
2026-03-02 16:50:15 +0300

    Header

--- src/widgets/header/ui/AppHeader.vue
+++ src/widgets/header/ui/AppHeader.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VSvg, VLink } from 'shuanvita-ui-kit'
+</script>
 
 <template>
   <header>
-
+    <VSvg name="logo" :width="209" />
+    <VLink href="#">Link</VLink>
   </header>
 </template>
